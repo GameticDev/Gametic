787aa5c28082ae3c5f5521d40efa3c7d7a022cea
GameticDev <gameticdev@gmail.com>
2025-05-15 23:07:17 +0530

    Revert "86cyxv7k8 (#4)" (#6)
    This reverts commit 1e9460f1577185941bb6fe5d39d43939b25f605a.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Outfit } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-const outfit = Outfit({
-  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -27,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={outfit.className}>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
