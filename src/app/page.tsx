0ca12844a4beb3d4955d6c9e8b6ec50ed575eea0
Aginas pk <aginaspk6@gmail.com>
2025-05-19 16:39:48 +0530

    LANDING

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import HeroSection from "@/components/landing/HeroSection";
 import HomePage from "@/components/landing/homePage";
+import ProvideBanner from "@/components/landing/ProvideBanner";
+import TopNavbar from "@/components/landing/TopNavbar";
+import Why from "@/components/landing/Why";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="bg-[#F0EFEB]">
       <div className="h-full relative">
-        <HomePage />
+        {/* <HomePage /> */}
+        <TopNavbar/>
+        <HeroSection/>
+        <Why/>
+        <ProvideBanner/>
       </div>
     </div>
   );
