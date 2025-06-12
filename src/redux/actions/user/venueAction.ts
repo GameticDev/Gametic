8e174a1c0e9b68e9dbe82a6a0ddfbebce2a0f4cc
GameticDev <gameticdev@gmail.com>
2025-06-12 09:40:03 +0530

    booking (#45)
    * landing
    
    * auth
    
    * eslintchnages
    
    * auth
    
    * auth
    
    * removed
    
    * full
    
    * booking
    
    * payemnt
    
    * payemnt
    
    * payemnt
    
    * ch
    
    * changes
    
    * eslint
    
    * venue
    
    * venuebook
    
    ---------
    
    Co-authored-by: Aginas pk <aginaspk6@gmail.com>

--- src/redux/actions/user/venueAction.ts
+++ src/redux/actions/user/venueAction.ts
@@ -1,4 +1,5 @@
-import { Venue } from "@/redux/slices/admin/venueSlice";
+import { Venue } from "@/redux/slices/user/venueSlice";
+import { Booking } from "@/types/turf";
 import axiosErrorManager from "@/utils/axiosErrorManager";
 import axiosInstance from "@/utils/axiosInstance";
 import { createAsyncThunk } from "@reduxjs/toolkit";
@@ -14,12 +15,46 @@ export const fetchAllVenues = createAsyncThunk<
       const response = await axiosInstance.get(
         `/getAllVenues?page=${page}&limit=${limit}&search=${search}`
       );
-      console.log(response.data)
+      console.log(response.data);
       return {
         allVenues: response.data.venues,
         totalVenues: response.data.totalVenues,
         totalActiveVenues: response.data.totalActiveVenues,
       };
+    } catch (error) {
+      return rejectWithValue(axiosErrorManager(error));
+    }
+  }
+);
+
+export const fetchVenueById = createAsyncThunk<
+  { turff: Venue },
+  { turfId: string | undefined },
+  { rejectValue: string }
+>("venuesUser/fetchVenueById", async ({ turfId }, { rejectWithValue }) => {
+  try {
+    const { data } = await axiosInstance.get(`/veunesById/${turfId}`);
+    return { turff: data.data };
+  } catch (error) {
+    return rejectWithValue(axiosErrorManager(error));
+  }
+});
+
+export const bookVenue = createAsyncThunk<
+  { booking: Booking },
+  { date: string; turfId: string; startTime: string; endTime: string },
+  { rejectValue: string }
+>(
+  "venuesUser/bookVenue",
+  async ({ date, turfId, startTime, endTime }, { rejectWithValue }) => {
+    try {
+      const { data } = await axiosInstance.post(`/venue-booking`, {
+        date,
+        turfId,
+        startTime,
+        endTime,
+      });
+      return { booking: data.booking };
     } catch (error) {
       return rejectWithValue(axiosErrorManager(error));
     }
